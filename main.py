9e3f77249c2aa9efd327b30a84de49c47b82d884
lionelgs <l.gilliar-schoenenb@cooper.edu>
2019-11-20 19:54:34 -0500

    Here is the complete functioning file. Thanks.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-
+import csv
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -7,7 +8,15 @@ def main():
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
+   # TODO: Fill in the actual logic here!
+    with open(input_file_path) as file:
+        reader =csv.reader(file, delimiter = ',')
+
+        for line in reader:
+            new_list = []
+            for item in line:
+                new_list.append(float(item))
+            print(mean(new_list))
 
 
 if __name__ == "__main__":
